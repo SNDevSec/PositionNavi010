ca2ed82c147347e9119f34e5144e8fbec0922a93
Hirokazu Kamachi <h.kamachi0507@gmail.com>
2018-12-31 00:44:25 +0900

    4

--- app/src/main/java/com/s_k/devsec/positionnavi010/CustomView.java
+++ app/src/main/java/com/s_k/devsec/positionnavi010/CustomView.java
@@ -6,22 +6,31 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
-import static java.lang.Math.floor;
 import static java.lang.Math.sqrt;
 
 public class CustomView extends View {
 
-    Paint paint;
-    Path path;
+    private Paint paint;
+    private Path path;
+    private boolean viewflg;
+    private double angle = 0;
 
     public CustomView(Context context, AttributeSet attrs){
         super(context, attrs);
         paint = new Paint();
         path = new Path();
+        viewflg = true;
+    }
+
+    public void showCanvas(boolean flg, double ang){
+        viewflg = flg;
+        angle = ang;
+        invalidate();
     }
 
     @Override
@@ -32,9 +41,13 @@ public class CustomView extends View {
 //        float yourYPosition = yourXPosition / 2 * 2 / (float)sqrt(3.0);
         float yourXPosition = customViewWidth / 2;
         float yourYPosition = customViewWidth * (float)0.7;
-        Log.i("CustomView", "yourYPosition:"+ yourYPosition);
-        float targetXPosition = 0;
-        float targetYPosition = 0;
+        Log.d("CustomView", "yourYPosition:"+ yourYPosition);
+        double radius = yourYPosition - (customViewWidth - yourYPosition);
+
+        float targetXPosition;
+        float targetYPosition;
+
+        RectF rectF = null;
 
         //背景
         canvas.drawColor(Color.argb(255, 255, 255, 255));
@@ -45,8 +58,7 @@ public class CustomView extends View {
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawPoint(yourXPosition,0, paint);
-//        canvas.drawPoint(yourXPosition/2,yourXPosition * (2 / (int)Math.sqrt(3.0)) * (2 / 5), paint);
-//        canvas.drawPoint(yourXPosition/2,yourXPosition /2, paint);
+
         //自分座標マーク
         canvas.drawCircle(yourXPosition, yourYPosition,10, paint);
 
@@ -56,13 +68,6 @@ public class CustomView extends View {
         paint.setStrokeWidth(5);
         paint.setTextSize(50);
         canvas.drawText("Your Position", yourXPosition, yourYPosition + 100, paint);
-
-        //ターゲットまでの点線表示(for文)
-//        paint.setStyle(Paint.Style.STROKE);
-//        paint.setStrokeWidth(1);
-//        for(int i = 0; i<=(int)yourYPosition - (int)targetYPosition; i=i+4){
-//            canvas.drawPoint(yourXPosition,targetYPosition + i, paint);
-//        }
 
         //レンジ線表示
         paint.setStrokeWidth(3);
@@ -75,27 +80,42 @@ public class CustomView extends View {
         path.lineTo(yourXPosition+((yourYPosition-(customViewWidth - yourYPosition))/2*(float)sqrt(3.0))*(float)1.3,yourYPosition-((yourYPosition-(customViewWidth - yourYPosition))/2)*(float)1.3);
         canvas.drawPath(path, paint);
 
+        //円弧
+        paint.setStrokeWidth(1);
+        rectF = new RectF(yourXPosition - (float)radius, yourYPosition - (float)radius, yourXPosition + (float)radius, yourYPosition + (float)radius);
+        canvas.drawArc(rectF, 210, 120, false, paint);
+
+        if(viewflg){
+            targetXPosition = yourXPosition;
+            targetYPosition = yourYPosition - (float)radius;
+        }else{
+            Log.d("CustomView", "radius:"+ radius);
+            Log.d("CustomView", "angle:"+ angle);
+            float rcos_theta = (float)(radius * Math.cos(Math.toRadians(angle)));
+            Log.d("CustomView", "rsin_theta:"+ rcos_theta);
+            float rsin_theta = (float)(radius * Math.sin(Math.toRadians(angle)));
+            Log.d("CustomView", "rcos_theta:"+ rsin_theta);
+            targetXPosition = yourXPosition + rsin_theta;
+            targetYPosition = yourYPosition - rcos_theta;
+        }
+
         //ターゲット座標マーク
-        targetXPosition = yourXPosition;
-        targetYPosition = customViewWidth - yourYPosition;
         paint.setColor(Color.BLUE);
         paint.setStrokeWidth(10);
         paint.setStyle(Paint.Style.STROKE);
         canvas.drawCircle(targetXPosition, targetYPosition,10, paint);
         //ターゲット座標文字
-//        paint.setStyle(Paint.Style.FILL_AND_STROKE);
         paint.setColor(Color.BLACK);
         paint.setStrokeWidth(5);
         paint.setTextSize(50);
         canvas.drawText("Target", targetXPosition, targetYPosition - 50, paint);
 
-        //ターゲットまでの点線表示
+        //ターゲットまでの線表示
         paint.setStrokeWidth(1);
 //        paint.setPathEffect(new DashPathEffect(new float[]{2, 4},0));
         path.rewind();
         path.moveTo(yourXPosition, yourYPosition);
         path.lineTo(targetXPosition,targetYPosition);
         canvas.drawPath(path, paint);
-
-    }
+   }
 }
--- app/src/main/java/com/s_k/devsec/positionnavi010/MainWindowActivity.java
+++ app/src/main/java/com/s_k/devsec/positionnavi010/MainWindowActivity.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainWindowActivity extends AppCompatActivity {
 
@@ -17,18 +18,53 @@ public class MainWindowActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
 
-        Button button1 = (Button) findViewById(R.id.btDemo1);
+        final TextView tvDistance = findViewById(R.id.tvDistance);
+        tvDistance.setText("0");
+        final TextView tvAngle = findViewById(R.id.tvAngle);
+        tvAngle.setText("0");
+
+        Button button1 = findViewById(R.id.btDemo1);
         button1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-
+                CustomView customView = findViewById(R.id.customView);
+                customView.showCanvas(false, 30);
+                Log.d("MainWindowActivity", "bt_number=1");
+                tvDistance.setText("10");
+                tvAngle.setText("30");
             }
         });
-        Button button2 = (Button) findViewById(R.id.btDemo2);
+        Button button2 = findViewById(R.id.btDemo2);
         button2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-
+                CustomView customView = findViewById(R.id.customView);
+                customView.showCanvas(false, 60);
+                Log.d("MainWindowActivity", "bt_number=2");
+                tvDistance.setText("20");
+                tvAngle.setText("60");
+            }
+        });
+        Button button3 = findViewById(R.id.btDemo3);
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                CustomView customView = findViewById(R.id.customView);
+                customView.showCanvas(false, -30);
+                Log.d("MainWindowActivity", "bt_number=3");
+                tvDistance.setText("5");
+                tvAngle.setText("-30");
+            }
+        });
+        Button button4 = findViewById(R.id.btReset);
+        button4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                CustomView customView = findViewById(R.id.customView);
+                customView.showCanvas(true, 0);
+                Log.d("MainWindowActivity", "bt_number=4");
+                tvDistance.setText("0");
+                tvAngle.setText("0");
             }
         });
     }
@@ -38,8 +74,8 @@ public class MainWindowActivity extends AppCompatActivity {
         super.onWindowFocusChanged(hasFocus);
         customViewWidth = findViewById(R.id.customView).getWidth();
         customViewHeight = findViewById(R.id.customView).getHeight();
-        Log.i("MainWindowActivity", "CustomView幅:"+ customViewWidth);
-        Log.i("MainWindowActivity", "CustomView高:"+ customViewHeight);
+        Log.d("MainWindowActivity", "CustomView幅:"+ customViewWidth);
+        Log.d("MainWindowActivity", "CustomView高:"+ customViewHeight);
 
         CustomView customView = (CustomView) findViewById(R.id.customView);
         ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams)customView.getLayoutParams();
